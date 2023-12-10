2e698bebfdeac1646b5abe7c2e25ac8cdb9317d6
leeboa2005 <leeboa2005@naver.com>
2023-12-10 21:55:04 +0900

    add images

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["00.jpg", "01.jpg" , "02.jpg", "03.jpg", "04.jpg"]
+const images = ["00.jpg", "01.jpg" , "02.jpg", "03.jpg", "04.jpg", "05.jpg" ,"06.jpg" ,"07.jpg"]
 const imageContainer = document.querySelector("#wrap")
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
